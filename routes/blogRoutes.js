c55ddfa437bbbca5d504fc6238991efd5966fdbe
David Zapata <dzr794@gmail.com>
2024-07-21 17:21:47 -0500

    separated all the views functions into a controllers file

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,59 +1,12 @@
 const express = require('express');
-const Blog = require('../_models/blog');
+const blogController = require('../controllers/blogController');
 
 const blogRouter = express.Router();
 
-
-
-blogRouter.get('/', (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
-    .then((result) => {
-      res.render('index', { title: 'All blogs', blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-blogRouter.post('/', (req, res) => {
-  console.table(req.body);
-  const blog = new Blog(req.body);
-  blog.save()
-    .then((result) => {
-      res.redirect('/blogs');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-blogRouter.get('/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
-
-blogRouter.get('/:id', (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render('details', { title: 'Blog Details', blog: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-});
-
-blogRouter.delete('/:id', (req, res) => {
-  const id = req.params.id;
-
-  Blog.findByIdAndDelete(id)
-    .then(result => {
-      res.json({ redirect: '/blogs' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-
+blogRouter.get('/', blogController.blog_index);
+blogRouter.post('/', blogController.blog_create_post);
+blogRouter.get('/create', blogController.blog_create_get);
+blogRouter.get('/:id', blogController.blog_details);
+blogRouter.delete('/:id', blogController.blog_delete);
 
 module.exports = blogRouter;
